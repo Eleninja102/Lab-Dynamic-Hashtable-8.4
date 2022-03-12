7fbfe4a17ff6f14fb0b7697aca57d4991e5ae9b4
Coleton Watt <electricninja102@gmail.com>
2022-03-12 15:02:53 -0700

    Initial Commit

---
+++ Lab: Dynamic Hashtable 8.4/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Lab: Dynamic Hashtable 8.4
+//
+//  Created by Coleton Watt on 3/12/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
